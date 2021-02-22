41b3f083542a6d2a7b22515eb80e36833a653b47
mfauzanalg <mfauzanalg@gmail.com>
2021-02-23 01:53:34 +0700

    decomposition js

---
+++ line.js
@@ -0,0 +1,5 @@
+var isDrawLine = false
+
+var drawLine = () => {
+  isDrawLine = true
+}
--- mouse.js
+++ mouse.js
@@ -1,26 +1,30 @@
 
 // Mouse Move
 var drag = false;
-var old_x, old_y;
+var x_one, x_two, y_one, y_two
 
 var mouseDown = function(e) {
+  console.log("DOWN")
   drag = true;
-  old_x = e.pageX, old_y = e.pageY;
+  x_two = e.pageX 
+  y_two = e.pageY;
+
   e.preventDefault();
   return false;
 };
 
 var mouseUp = function(e){
+  console.log("UP")
+  // render()
   drag = false;
 };
 
 var mouseMove = function(e) {
-  old_x = e.pageX
-  old_y = e.pageY
+  x_one = e.pageX
+  y_one = e.pageY
   e.preventDefault();
 };
 
 canvas.addEventListener("mousedown", mouseDown, false);
 canvas.addEventListener("mouseup", mouseUp, false);
-canvas.addEventListener("mouseout", mouseUp, false);
 canvas.addEventListener("mousemove", mouseMove, false);
