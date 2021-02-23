6ffa644f7bc83c34d1af3b48ed2980436e6546f8
mfauzanalg <mfauzanalg@gmail.com>
2021-02-23 22:07:55 +0700

    draw square

--- mouse.js
+++ mouse.js
@@ -13,11 +13,48 @@ var mouseDown = function(e) {
   if(geoObject != ''){
     x_one = e.pageX 
     y_one = e.pageY;
-    isDrawing = !isDrawing;
-    e.preventDefault();
-    return false;
+
+    if (geoObject == "line"){
+      isDrawing = !isDrawing;
+    }
   }
+
+  if (!isDrawing && geoObject == "square"){
+    renderSquare()
+    const shape = {
+      method: drawMethod,
+      vertices: verticesArr
+    }
+
+    allShapes.push(shape)
+    renderAll()
+    geoObject = ""
+  }
+
+  e.preventDefault();
+  return false;
 };
+
+var renderSquare = () => {
+  const size = 80
+    x_one = x_one - size/2
+    y_one = y_one + size/2
+    x_two = x_one + size
+    y_two = y_one
+
+    drawMethod = gl.TRIANGLE_STRIP
+    verticesArr = [
+      getCoorX(x_one), 
+      getCoorY(y_one), 
+      getCoorX(x_two), 
+      getCoorY(y_two),
+
+      getCoorX(x_one), 
+      getCoorY(y_one - size), 
+      getCoorX(x_two), 
+      getCoorY(y_two - size),
+    ]
+}
 
 var mouseMove = function(e) {
   x_two = e.pageX
@@ -32,9 +69,17 @@ var mouseMove = function(e) {
         getCoorX(x_two), 
         getCoorY(y_two),
       ]
-      renderAll(drawMethod, verticesArr)
-      render(drawMethod, verticesArr)
     }
+    renderAll()
+    render(drawMethod, verticesArr)
+  }
+
+  if (geoObject == "square"){
+    x_one = e.pageX
+    y_one = e.pageY
+    renderSquare()
+    render(drawMethod, verticesArr)
+    renderAll()
   }
 };
 
@@ -46,7 +91,7 @@ var mouseUp = function(e){
     }
     
     allShapes.push(shape)
-    renderAll(drawMethod, verticesArr)
+    renderAll()
     geoObject = ""
   }
 };
